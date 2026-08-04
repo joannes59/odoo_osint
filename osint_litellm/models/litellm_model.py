fb26c909e1723134b3a13574fa19b638f6dfb6e2
joannes59 <joannes.landy@gmail.com>
2026-08-04 13:40:02 +0200

    save

--- osint_litellm/models/litellm_model.py
+++ osint_litellm/models/litellm_model.py
@@ -8,7 +8,8 @@ Created on Wed Jul  8 19:10:31 2026
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError
-
+from litellm import get_supported_openai_params, get_model_info
+import json
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -37,7 +38,27 @@ class LitellmModel(models.Model):
     context_length = fields.Integer('Context Length')
     embedding_length = fields.Integer('Embedding Length')
     capability_ids = fields.Many2many('litellm.model.capability', string='Capabilities')
+    response_format = fields.Boolean('Response Format')
+    json_schema = fields.Boolean('JSON Schema')
     
+    description_pretty = fields.Text(
+            string="Description",
+            compute="_compute_description_pretty"
+        )
+
+    @api.depends("description")
+    def _compute_description_pretty(self):
+        for rec in self:
+            description_pretty = ""
+            
+            if rec.description:
+                description_pretty = json.dumps(
+                        rec.description,
+                        indent=4,
+                        ensure_ascii=False
+                    )
+            rec.description_pretty = description_pretty
+                    
     def get_apikey(self):
         """ Return api key to use with this model, depend on user """
         self.ensure_one()
@@ -60,4 +81,33 @@ class LitellmModel(models.Model):
             api_key = api_key_ids[0].key
 
         return api_key
+
+    def action_update_model_support(self):
+        """ Update response_format and json_schema fields from litellm."""
+
+        for model in self:
+            custom_llm_provider = model.provider_id.litellm_provider.lower()
+    
+            try:
+                params = get_supported_openai_params(
+                    model=model.model,
+                    custom_llm_provider=custom_llm_provider,
+                )
+                model.response_format = 'response_format' in params
+                print(model.model,type(params), params)
+                
+                info = get_model_info(
+                    model=model.model,
+                    custom_llm_provider=custom_llm_provider,
+                    )
+                print('-------------------------')
+                print(info)
+                
+                
+            except Exception as e:
+                raise UserError("Failed to get supported params: %s" % str(e))
+    
+
+
+
         
